ed4f2b7ce95239105680a046e596608744f95078
Andrey Campassi <andrey.campassi@movile.com>
2014-01-20 15:39:18 -0200

    Memes

---
+++ teste.py~
@@ -0,0 +1,10 @@
+import math
+
+i = 1.02
+t = 12
+m = i**t
+c = 2000
+m = m*c
+
+print m
+
---
+++ teste2.py
@@ -0,0 +1,10 @@
+import math
+t = 1
+while t < 12:
+	i = 1.02
+	t = t+1
+	m = i**t
+	c = 2000
+	m = m*c
+	print "Mes: "+t+" Montante:" + m
+
